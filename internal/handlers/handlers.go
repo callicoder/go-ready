d32ab54f99eed30523e044336666c98f03337872
Rajeev Kumar Singh <callicoder@gmail.com>
2019-02-15 16:11:16 +0530

    cleanup

--- internal/handlers/handlers.go
+++ internal/handlers/handlers.go
@@ -2,32 +2,20 @@ package handlers
 
 import (
 	"net/http"
+
+	"github.com/callicoder/go-ready/pkg/errors"
 )
 
 type Handler struct {
 	HandleFunc             func(*Context, http.ResponseWriter, *http.Request)
-	IsStatic               bool
 	RequiresAuthentication bool
 }
 
 func (h Handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	c := NewContext(w, r)
 
-	if h.IsStatic {
-		// Instruct the browser not to display us in an iframe unless is the same origin for anti-clickjacking
-		w.Header().Set("X-Frame-Options", "SAMEORIGIN")
-		w.Header().Set("Content-Security-Policy", "frame-ancestors 'self'")
-	} else {
-		// All api response bodies will be JSON formatted by default
-		w.Header().Set("Content-Type", "application/json")
-
-		if r.Method == http.MethodGet {
-			w.Header().Set("Expires", "0")
-		}
-	}
-
 	if h.RequiresAuthentication && c.Session == nil {
-		w.WriteHeader(http.StatusUnauthorized)
+		c.Error(errors.NewUnauthorizedError(errors.New("Sorry, You're not authorized to access this resource")))
 		return
 	}
 
@@ -37,7 +25,6 @@ func (h Handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 func ApiHandler(h func(*Context, http.ResponseWriter, *http.Request)) http.Handler {
 	return &Handler{
 		HandleFunc:             h,
-		IsStatic:               false,
 		RequiresAuthentication: false,
 	}
 }
@@ -45,7 +32,6 @@ func ApiHandler(h func(*Context, http.ResponseWriter, *http.Request)) http.Handl
 func ApiAuthenticatedHandler(h func(*Context, http.ResponseWriter, *http.Request)) http.Handler {
 	return &Handler{
 		HandleFunc:             h,
-		IsStatic:               false,
 		RequiresAuthentication: true,
 	}
 }
